2bbfa02f08869eb757c1939c416b9038cb632b19
Koushik Dutta <koush@koushikdutta.com>
2023-06-23 10:46:06 -0700

    Revert "Revert "Re-add config flow and fix setup from #2""

--- custom_components/scrypted/config_flow.py
+++ custom_components/scrypted/config_flow.py
@@ -6,10 +6,11 @@ from typing import Any, cast
 
 import voluptuous as vol
 
-from homeassistant.components.sensor import DOMAIN as SENSOR_DOMAIN
-from homeassistant.const import CONF_ENTITY_ID
+from homeassistant.const import CONF_HOST
+from homeassistant.core import callback
 from homeassistant.helpers import selector
 from homeassistant.helpers.schema_config_entry_flow import (
+    SchemaCommonFlowHandler,
     SchemaConfigFlowHandler,
     SchemaFlowFormStep,
     SchemaFlowMenuStep,
@@ -17,26 +18,27 @@ from homeassistant.helpers.schema_config_entry_flow import (
 
 from .const import DOMAIN
 
-OPTIONS_SCHEMA = vol.Schema(
+
+async def validate_host(
+    flow: SchemaCommonFlowHandler, data: dict[str, Any]
+) -> dict[str, Any]:
+    """Validate that the host is valid."""
+    await flow.parent_handler.async_set_unique_id(data[CONF_HOST])
+    flow.parent_handler._abort_if_unique_id_configured()
+    return data
+
+
+CONFIG_SCHEMA = vol.Schema(
     {
-        vol.Required(CONF_ENTITY_ID): selector.EntitySelector(
-            selector.EntitySelectorConfig(domain=SENSOR_DOMAIN)
+        vol.Required(CONF_HOST): selector.TextSelector(
+            selector.TextSelectorConfig(type=selector.TextSelectorType.URL)
         ),
     }
 )
 
-CONFIG_SCHEMA = vol.Schema(
-    {
-        vol.Required("name"): selector.TextSelector(),
-    }
-).extend(OPTIONS_SCHEMA.schema)
-
 CONFIG_FLOW: dict[str, SchemaFlowFormStep | SchemaFlowMenuStep] = {
-    "user": SchemaFlowFormStep(CONFIG_SCHEMA)
-}
-
-OPTIONS_FLOW: dict[str, SchemaFlowFormStep | SchemaFlowMenuStep] = {
-    "init": SchemaFlowFormStep(OPTIONS_SCHEMA)
+    "user": SchemaFlowFormStep(CONFIG_SCHEMA, validate_user_input=validate_host),
+    "import": SchemaFlowFormStep(CONFIG_SCHEMA, validate_user_input=validate_host),
 }
 
 
@@ -45,6 +47,7 @@ class ConfigFlowHandler(SchemaConfigFlowHandler, domain=DOMAIN):
 
     config_flow = CONFIG_FLOW
 
+    @callback
     def async_config_entry_title(self, options: Mapping[str, Any]) -> str:
         """Return config entry title."""
-        return cast(str, options["name"]) if "name" in options else ""
+        return cast(str, options[CONF_HOST])
